7fffc9504e3eaa151d6f6eff4f92874957cd37cb
Claudio Matos <cloud.devmob@gmail.com>
2021-10-30 01:21:46 -0300

    first commit

---
+++ src/pages/components/User.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface IUser {
+    name: string,
+    email: string,
+}
+
+interface Props {
+    user: IUser;
+}
+
+const User: React.FC<Props> = ({ user }) => (
+    <div>
+        <strong>Nome:</strong> {user.name} <br />
+        <strong>E-mail:</strong> {user.email} <br />
+    </div>
+)
+
+export default User;
